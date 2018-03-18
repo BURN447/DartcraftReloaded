64214521c4f147ff299544d2aa252a80822d38d3
Unknown <herobrine098@gmail.com>
2018-03-17 23:10:21 -0700

    ForceAxe Class Creation
    Constructor

--- src/main/java/burn447/dartcraftReloaded/Items/Tools/ForceAxe.java
+++ src/main/java/burn447/dartcraftReloaded/Items/Tools/ForceAxe.java
@@ -3,11 +3,11 @@ package burn447.dartcraftReloaded.Items.Tools;
 /**
  * Created by BURN447 on 3/6/2018.
  */
-public class ItemAxe extends net.minecraft.item.ItemAxe {
+public class ForceAxe extends net.minecraft.item.ItemAxe {
 
     private String name;
 
-    public ItemAxe(ToolMaterial material, String name){
+    public ForceAxe(ToolMaterial material, String name){
         super(material);
         setUnlocalizedName(name);
         this.name = name;
