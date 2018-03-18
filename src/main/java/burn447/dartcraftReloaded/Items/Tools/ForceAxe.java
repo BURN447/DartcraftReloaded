fa557440addc317e06f6dd99a6b8e5d074d5748e
Unknown <herobrine098@gmail.com>
2018-03-18 00:20:04 -0700

    Removed Bugged Tool Code

--- src/main/java/burn447/dartcraftReloaded/Items/Tools/ForceAxe.java
+++ src/main/java/burn447/dartcraftReloaded/Items/Tools/ForceAxe.java
@@ -1,5 +1,9 @@
 package burn447.dartcraftReloaded.Items.Tools;
 
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
+
 /**
  * Created by BURN447 on 3/6/2018.
  */
@@ -11,7 +15,16 @@ public class ForceAxe extends net.minecraft.item.ItemAxe {
         super(material);
         setUnlocalizedName(name);
         this.name = name;
+        hasSubtypes = true;
     }
+
+    public void getSubItems(CreativeTabs tab, NonNullList<ItemStack> items){
+        if(this.isInCreativeTab(tab)){
+
+        }
+    }
+
+
 
 
 }
---
+++ src/main/java/burn447/dartcraftReloaded/util/capablilities/ITool.java
@@ -0,0 +1,13 @@
+package burn447.dartcraftReloaded.util.capablilities;
+
+import burn447.dartcraftReloaded.util.References;
+import net.minecraft.item.ItemStack;
+
+/**
+ * Created by BURN447 on 3/18/2018.
+ */
+public interface ITool {
+
+    void addModifier(References.MODIFIERS mod, ItemStack tool);
+
+}
