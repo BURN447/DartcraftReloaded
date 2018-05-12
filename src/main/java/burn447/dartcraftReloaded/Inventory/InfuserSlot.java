915f579785b0fb2616212bd9e88f4840afc6f841
Unknown <Kyle.e.newton@wsu.edu>
2018-05-12 09:25:54 -0700

    Possible Custom Slot for the Infuser

---
+++ src/main/java/burn447/dartcraftReloaded/Inventory/InfuserSlot.java
@@ -0,0 +1,46 @@
+package burn447.dartcraftReloaded.Inventory;
+
+import burn447.dartcraftReloaded.Blocks.ModBlocks;
+import burn447.dartcraftReloaded.Items.ModItems;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.Slot;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
+
+/**
+ * Created by BURN447 on 5/8/2018.
+ */
+public class InfuserSlot extends Slot {
+
+    private NonNullList<Item> validInfuserModifiers = populateModifierList();
+
+    public InfuserSlot(IInventory inventoryIn, int index, int xPosition, int yPosition){
+        super(inventoryIn, index, xPosition, yPosition);
+    }
+
+    @Override
+    public boolean isItemValid(ItemStack item){
+
+        for(Item stack : validInfuserModifiers) {
+            if (item.getItem() == stack) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private NonNullList<Item> populateModifierList(){
+        NonNullList<Item> list = NonNullList.<Item>withSize(25, null);
+
+        list.add(ModItems.nuggetForce);
+        list.add(Items.SUGAR);
+        list.add(ModBlocks.forceLog.createItemBlock());
+        list.add(Items.FLINT);
+        list.add(Items.EXPERIENCE_BOTTLE);
+
+        return list;
+    }
+}
