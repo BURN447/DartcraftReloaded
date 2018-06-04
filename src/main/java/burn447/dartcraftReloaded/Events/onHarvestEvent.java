aa7bde557b7a94db18f071fed3bb51e084f4ead9
BURN447 <Herobrine098@gmail.com>
2018-06-04 16:29:52 -0700

    Fixed Crash with onBlockHarvested event

--- src/main/java/burn447/dartcraftReloaded/Events/onHarvestEvent.java
+++ src/main/java/burn447/dartcraftReloaded/Events/onHarvestEvent.java
@@ -16,30 +16,32 @@ import static burn447.dartcraftReloaded.Handlers.DCRCapabilityHandler.CAPABILITY
 public class onHarvestEvent {
 
     @SubscribeEvent
-    public void onHarvestDrops(BlockEvent.HarvestDropsEvent event){
-        ItemStack heldItem = event.getHarvester().inventory.getCurrentItem();
+    public void onHarvestDrops(BlockEvent.HarvestDropsEvent event) {
+        ItemStack heldItem;
+        if(event.getHarvester() != null){
+            heldItem = event.getHarvester().inventory.getCurrentItem();
+            if (heldItem != ItemStack.EMPTY) {
+                if (heldItem.getCapability(CAPABILITY_TOOLMOD, null) != null) {
+                    if (heldItem.getCapability(CAPABILITY_TOOLMOD, null).hasHeat()) {
+                        ListIterator<ItemStack> iter = event.getDrops().listIterator();
+                        while (iter.hasNext()) {
+                            ItemStack drop = iter.next();
+                            ItemStack smelted = FurnaceRecipes.instance().getSmeltingResult(drop);
+                            if (!smelted.isEmpty()) {
+                                smelted = smelted.copy();
+                                smelted.setCount(drop.getCount());
 
-        if(heldItem != null){
-            if(heldItem.getCapability(CAPABILITY_TOOLMOD, null) != null){
-                if(heldItem.getCapability(CAPABILITY_TOOLMOD, null).hasHeat()){
-                    ListIterator<ItemStack> iter = event.getDrops().listIterator();
-                    while(iter.hasNext()){
-                        ItemStack drop = iter.next();
-                        ItemStack smelted = FurnaceRecipes.instance().getSmeltingResult(drop);
-                        if(!smelted.isEmpty()){
-                            smelted = smelted.copy();
-                            smelted.setCount(drop.getCount());
+                                //TODO: Add Fortune Integration
 
-                            //TODO: Add Fortune Integration
+                                iter.set(smelted);
 
-                            iter.set(smelted);
-
-                            float xp = FurnaceRecipes.instance().getSmeltingExperience(smelted);
-                            if(xp < 1 && Math.random() < xp){
-                                xp += 1F;
-                            }
-                            if(xp >= 1F){
-                                event.getState().getBlock().dropXpOnBlockBreak(event.getWorld(), event.getPos(), (int) xp);
+                                float xp = FurnaceRecipes.instance().getSmeltingExperience(smelted);
+                                if (xp < 1 && Math.random() < xp) {
+                                    xp += 1F;
+                                }
+                                if (xp >= 1F) {
+                                    event.getState().getBlock().dropXpOnBlockBreak(event.getWorld(), event.getPos(), (int) xp);
+                                }
                             }
                         }
                     }
