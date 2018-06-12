3d8cc7b395230917d27e81da1cebda06b032a50f
BURN447 <Herobrine098@gmail.com>
2018-06-11 17:35:35 -0700

    Experience Tome Capability and Item

---
+++ src/main/java/burn447/dartcraftReloaded/Items/ItemExperienceTome.java
@@ -0,0 +1,7 @@
+package burn447.dartcraftReloaded.Items;
+
+/**
+ * Created by BURN447 on 6/11/2018.
+ */
+public class ItemExperienceTome {
+}
---
+++ src/main/java/burn447/dartcraftReloaded/capablilities/ExperienceTome/ExperienceTomeFactory.java
@@ -0,0 +1,7 @@
+package burn447.dartcraftReloaded.capablilities.ExperienceTome;
+
+/**
+ * Created by BURN447 on 6/10/2018.
+ */
+public class ExperienceTomeFactory {
+}
