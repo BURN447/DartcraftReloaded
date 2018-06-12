000f3d27784b82d76635efd3777b1f523f6e60eb
BURN447 <Herobrine098@gmail.com>
2018-06-11 21:27:49 -0700

    Force Pack Addition
    Doesn't do anything - Need to work with the UUID system

---
+++ src/main/java/burn447/dartcraftReloaded/Items/ItemForcePack.java
@@ -0,0 +1,11 @@
+package burn447.dartcraftReloaded.Items;
+
+/**
+ * Created by BURN447 on 6/11/2018.
+ */
+public class ItemForcePack extends ItemBase {
+
+    public ItemForcePack(String name){
+        super(name);
+    }
+}
