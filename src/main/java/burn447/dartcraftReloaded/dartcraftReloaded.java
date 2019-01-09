f695ab6d3cf6280f766c0965df3ec19d11e433e6
BURN447 <Herobrine098@gmail.com>
2019-01-08 23:54:59 -0800

    Fixed Weak Armor Resistance

--- src/main/java/burn447/dartcraftReloaded/dartcraftReloaded.java
+++ src/main/java/burn447/dartcraftReloaded/dartcraftReloaded.java
@@ -44,7 +44,7 @@ public class dartcraftReloaded {
     @SidedProxy(serverSide = "burn447.dartcraftReloaded.proxy.CommonProxy", clientSide = "burn447.dartcraftReloaded.proxy.ClientProxy")
     public static CommonProxy proxy;
 
-    public static final ItemArmor.ArmorMaterial forceArmorMaterial = EnumHelper.addArmorMaterial("FORCE", modId + ":force", 15, new int[]{1, 2, 3, 1}, 0, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER, 0.0F);
+    public static final ItemArmor.ArmorMaterial forceArmorMaterial = EnumHelper.addArmorMaterial("FORCE", modId + ":force", 15, new int[]{1, 2, 3, 1}, 0, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 4.0F);
     public static final ItemTool.ToolMaterial forceToolMaterial = EnumHelper.addToolMaterial("FORCE", 3, 1561, 8.0F, 8.0F, 22);
 
     static {
