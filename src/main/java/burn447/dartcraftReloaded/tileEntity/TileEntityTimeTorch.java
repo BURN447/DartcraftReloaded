bb55ff8d7b6a1ee504cba3352124f36b6c480516
BURN <herobrine098@gmail.com>
2019-01-25 15:26:10 -0800

    Beginnings of TimeTorch

---
+++ src/main/java/burn447/dartcraftReloaded/tileEntity/TileEntityTimeTorch.java
@@ -0,0 +1,11 @@
+package burn447.dartcraftReloaded.tileEntity;
+
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ITickable;
+
+public class TileEntityTimeTorch extends TileEntity implements ITickable {
+    @Override
+    public void update() {
+        
+    }
+}
