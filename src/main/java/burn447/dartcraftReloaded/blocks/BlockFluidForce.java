ceb8124201de849d6d1943a98c9cfdcc3a76b5be
BURN447 <Herobrine098@gmail.com>
2019-03-02 15:44:20 -0800

    Fluid force now gives Regen 10 when stood in

--- src/main/java/burn447/dartcraftReloaded/blocks/BlockFluidForce.java
+++ src/main/java/burn447/dartcraftReloaded/blocks/BlockFluidForce.java
@@ -1,5 +1,13 @@
 package burn447.dartcraftReloaded.blocks;
 
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.init.MobEffects;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.fluids.BlockFluidClassic;
 
 import static burn447.dartcraftReloaded.Fluids.ModFluids.fluidForce;
@@ -13,5 +21,12 @@ public class BlockFluidForce extends BlockFluidClassic {
         super(fluidForce, fluidForce.getMaterial());
         this.setRegistryName("fluidForce");
         this.setTranslationKey("fluid_force");
+    }
+
+    @Override
+    public void onEntityCollision(World worldIn, BlockPos pos, IBlockState state, Entity entityIn) {
+        if(entityIn instanceof EntityLivingBase) {
+            ((EntityLivingBase) entityIn).addPotionEffect(new PotionEffect(MobEffects.REGENERATION, 10, 10));
+        }
     }
 }
